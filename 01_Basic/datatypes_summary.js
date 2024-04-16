37a973242988412023de3992e4b4bbb737502361
rahulshinde1203 <rahulshinde2k@gmail.com>
2024-04-16 18:40:56 +0530

    scz

---
+++ 01_Basic/datatypes_summary.js
@@ -0,0 +1,10 @@
+//*******************DataTypes************************
+
+
+// 1 Primitive
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+
+//2 Non Premitive or Reference 
+//Array, Object, Functions
