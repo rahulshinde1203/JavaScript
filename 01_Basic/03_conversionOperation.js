8545576a3912c47ab2ba3391e7478e28e5d2bd2a
rahulshinde1203 <rahulshinde2k@gmail.com>
2024-04-16 17:35:24 +0530

    conversion complete part 2

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -17,3 +17,29 @@ console.table([isLoggedIn, BooleanIsLogIn])
 let someNumber = 33
 let stringnumber = String(someNumber)
 console.table([someNumber,stringnumber, typeof(stringnumber)])
+
+//**************** Operations*******************
+ let value = 3
+ let negvalue = -value
+ console.table([value,negvalue,])
+
+ // operation 
+ //Addtion (a+b)
+ //Substraction (a-b)
+ //Multiplication (a*b)
+ //Power (a**b)
+ //Division (a/b)
+ //remender (a%b)
+
+
+ let str1= "hello"
+ let str2 = "Rahul"
+
+ let str3 = str1+str2 // concatation
+console.log(str3);
+
+console.log("1"+2); // output 12
+
+console.log(1+"2"); // output 12
+console.log("1"+2+2); // output 122
+console.log(1+2+"2"); // output 32
