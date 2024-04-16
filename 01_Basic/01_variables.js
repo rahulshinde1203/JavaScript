93faa23845946600be18d388f5af841515bfb9a9
rahulshinde1203 <rahulshinde2k@gmail.com>
2024-04-16 16:38:21 +0530

    01_Variable complete

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail="xyz@mail.com"
+var accountPass = "12345678"
+let accountState;
+
+console.table([accountEmail, accountId, accountPass,accountState])
+console.log(accountId);
+
+/*
+Prefer not use var keyword
+beacuse of scope
+*/
