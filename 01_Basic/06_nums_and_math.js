ce534cadb4245036fad4f369984d164733ba63e1
rahulshinde1203 <rahulshinde2k@gmail.com>
2024-04-20 19:06:01 +0530

    num and math

---
+++ 01_Basic/06_nums_and_math.js
@@ -0,0 +1,30 @@
+const score = 400
+console.log(score);
+
+const num = new Number(200)
+console.log(num.toString());
+console.log(num.toFixed(2));
+
+const otherNumber = 123.3434345
+console.log(otherNumber.toPrecision(4));
+
+const hundread = 10000000000
+console.log(hundread.toLocaleString('en-IN'));
+
+
+//Maths
+
+console.log(Math)
+console.log(Math.abs(-223));
+console.log(Math.round(4.345));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min= 10
+const max = 20
+
+console.log(Math.floor(Math.random()));
+
+
+
