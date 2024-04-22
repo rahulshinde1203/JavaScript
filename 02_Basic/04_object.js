8ff7fd05c38daab5577551aab4a7e4469cd2a05b
rahulshinde1203 <rahulshinde2k@gmail.com>
2024-04-22 20:16:56 +0530

    04 Object

---
+++ 02_Basic/04_object.js
@@ -0,0 +1,44 @@
+const tinderUser = new Object();
+
+tinderUser.id = "123abc";
+tinderUser.name = "Sammy";
+tinderUser.isLogIn = false;
+console.log(tinderUser);
+
+const regularUser = {
+  email: "abc@mail.com",
+  fullName: {
+    userFullName: {
+      firstName: "Rahul",
+      lastName: "Shinde",
+    },
+  },
+};
+
+console.log(regularUser.fullName);
+
+const obj1 ={
+    1:"a",
+    2: "b"
+}
+
+const obj2 = {
+    3: "c",
+    4: "d"
+}
+
+const obj3 = {obj1, obj2}
+console.log(obj3);
+
+const obj4 = Object.assign({}, obj1, obj2)
+console.log(obj4);
+
+const obj6 = {...obj1, ...obj2}
+console.log(obj6);
+
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLogIn'));
