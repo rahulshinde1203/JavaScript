fc2a18cbead474c734c8468803e934a23e46fa93
rahulshinde1203 <rahulshinde2k@gmail.com>
2024-04-23 17:44:47 +0530

    scope coplete

--- 03_Basic/02_scopes.js
+++ 03_Basic/02_scopes.js
@@ -7,3 +7,41 @@ var c = 30
 console.log(a);
 console.log(b);
 console.log(c);
+
+
+function one(){
+    const username = "Rahul"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+
+if(true){
+    const username = "Rahul"
+    if(username=== "Rahul"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+//***************interesting***************/
+
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
